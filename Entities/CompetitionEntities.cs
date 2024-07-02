c254706a4d9d792beaee37491e069595bc1ebe6c
MMDavito <davidking9753@gmail.com>
2024-07-02 16:07:59 +0200

    Initial Commit

---
+++ Entities/CompetitionEntities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+// add a reference to System.ComponentModel.DataAnnotations DLL
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+
+
+namespace Entities
+{
+    public class CompetitionEntities : DbContext
+    {
+        public DbSet<Competition> Competitions { get; set; }
+    }
+
+    public class Competition
+    {
+        public int CompetitionId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Competitor> Competitors { get; set; } = new List<Competitor>();
+    }
+    public class Competitor
+    {
+        public int CompetitorId { get; set; }
+        public string Name { get; set; }
+        public int CompetitionId { get; set; }
+        public virtual CompetitionEntities Competition { get; set; } = null!;
+    }
+}
---
+++ Libraries/CompetitionLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries
+{
+    public class CompetitionLibrary
+    {
+        /*
+         Will contain: 
+            En metod som hämtar en lista med alla tävlingar i databasen.
+            En metod som kan hämta en specifik tävling, tar ett id som parameter.
+            
+            Använd LINQ för dessa ”queries”.
+         */
+    }
+}
