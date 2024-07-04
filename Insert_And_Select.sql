9998cccde759b9a88ab197d150ff48785c957ff7
MMDavito <davidking9753@gmail.com>
2024-07-04 19:21:41 +0200

    Cleaned .sql file

--- Insert_And_Select.sql
+++ Insert_And_Select.sql
@@ -1,15 +1,11 @@
 USE systematiq_competition;
-USE master;
-DROP DATABASE systematiq_competition;
-/*
+
 --INSERT INTO Competitions(Name) VALUES('TEST');
 SELECT * FROM Competitions;
 SELECT * FROM Competitors ORDER BY CompetitorId ASC;
-
 
 SELECT Competitions.CompetitionId, Competitions.Name, Competitors.CompetitorId, Competitors.Name  
 FROM Competitions
 LEFT JOIN Competitors 
 	ON Competitions.CompetitionId = Competitors.CompetitionId
 ORDER BY Competitions.CompetitionId;
-*/
